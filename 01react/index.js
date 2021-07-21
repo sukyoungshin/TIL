885b9a28d350acb424977359be784a11a385b999
SuKyoung <87718391+sukyoungshin@users.noreply.github.com>
2021-07-21 11:30:47 +0900

    함수형 컴포넌트 예문

---
+++ 01react/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SubwayLine from './components/Gpp'; 
+
+ReactDOM.render(
+  // <RoundBtn id="btn">버튼</RoundBtn>,
+  <SubwayLine />,
+  document.getElementById('root')
+);
