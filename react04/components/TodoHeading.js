cb621a030373ce7fcca2bcc37e8b500fa02b19d2
SuKyoung <87718391+sukyoungshin@users.noreply.github.com>
2021-07-26 12:40:23 +0900

    UPDATE: 코드 수정

--- react04/components/TodoHeading.js
+++ react04/components/TodoHeading.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'; // styled: 컴포넌트를 만들어주는 모듈
 
-const Heading = styled.h1`font-size:48px; margin:0;`;
 const Header = styled.header`
   width: 100vw; 
   height: 100px;
@@ -9,6 +8,10 @@ const Header = styled.header`
   color: white;
   text-align: center;
   padding-top: 25px;
+`;
+const Heading = styled.h1`
+  font-size:48px; 
+  margin:0;
 `;
 
 const ToDoHeading = () => {
