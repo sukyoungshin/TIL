cb251c7b673112ea5c358d68a305f614c0a6cd77
SuKyoung <87718391+sukyoungshin@users.noreply.github.com>
2021-07-26 13:10:12 +0900

    UPDATE: 주석추가

--- react01/components/App.js
+++ react01/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; // 해당 파일에서 JSX 문법을 사용하기 위해 React를 import 한다.
 
 const myStyle = {
   color: 'skyblue',
@@ -7,7 +7,7 @@ const myStyle = {
   textAlign: 'center',
 };
 
-// export 하지않는 함수형 컴포넌트 만들기 (내부 함수)
+// export 하지않는 함수형 컴포넌트 (내부 함수)
 function Book(){
   return <h1>BOOK</h1>
 };
