a7bc4c5023966a4328997c8c14f616ecd8401a4e
SuKyoung <87718391+sukyoungshin@users.noreply.github.com>
2021-08-03 08:11:11 +0900

    Add files via upload

---
+++ SCSS/Bpp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+const Dpp = () => {
+  return (
+  <>
+    <h1>HEADING</h1>
+    <p>PARAGRAPH <span>SPAN</span> <strong>STRONG</strong></p>
+  </>
+  );
+};
+
+export default Dpp;
